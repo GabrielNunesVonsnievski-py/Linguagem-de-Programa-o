7aa43c671418016bc549501f5af533cec431f3e9
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:22:48 -0300

    Update ex2.py

--- lista 1/ex2.py
+++ lista 1/ex2.py
@@ -1,3 +1,4 @@
+"2) Faça um script que leia um número e mostre o seu dobro, seu triplo e a sua raiz quadrada."
 n = int(input(f"Digite um número: "))
 print(f"Seu dobro é: ",n * 2)
 print(f"Seu triplo é: ",n * 3)
