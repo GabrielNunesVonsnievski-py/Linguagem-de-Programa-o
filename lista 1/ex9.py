0ba2b59a871417dbbdd01e30e69f014f317f611e
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 15:34:50 -0300

    ex9

---
+++ lista 1/ex9.py
@@ -0,0 +1,6 @@
+nome = input(f"Digite seu nome: ")
+dinheiro = int(input(f"Digite a quantia do seu dinheiro: "))
+dollar = dinheiro * 4,98
+euro = dinheiro * 5,43
+print(f"O valor em Dóllares é: ",dollar)
+print(f"O valor em Euros é: ",euro)
