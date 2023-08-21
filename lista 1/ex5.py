961d60b8bcf1ddad30b21ede1b6093fa2e175324
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 15:00:41 -0300

    ex5

---
+++ lista 1/ex5.py
@@ -0,0 +1,6 @@
+metros = int(input("Digite um valor em Metros: "))
+centimetros = metros /100
+milimetros = metros /1000
+
+print(f"O mesmo valor em centímetros é:",centimetros)
+print(f"O mesmo valor em milímetros é:",milimetros)
