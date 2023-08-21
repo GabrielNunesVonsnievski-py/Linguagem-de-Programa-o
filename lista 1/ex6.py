8fb5e898787b64cd26dd1665f9cf6335118995f4
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 15:07:57 -0300

    ex6

---
+++ lista 1/ex6.py
@@ -0,0 +1,4 @@
+carteira = float(input("Digite o valor de reais em sua carteira: "))
+dollar = carteira /5.41
+
+print(f"Com esse dinheiro na sua carteira você pode comprar {round(dollar, 2)} dólares.")
