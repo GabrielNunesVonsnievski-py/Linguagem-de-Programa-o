9663d9903e500412fee995503f5d3947881c2224
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:26:43 -0300

    Update ex6.py

--- lista 1/ex6.py
+++ lista 1/ex6.py
@@ -1,3 +1,6 @@
+"6) Faça um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode
+comprar."
+
 carteira = float(input("Digite o valor de reais em sua carteira: "))
 dollar = carteira /5.41
 
