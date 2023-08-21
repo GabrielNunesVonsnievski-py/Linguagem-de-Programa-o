01fca80420a8ce12c6e595a7d8b6da44aa35f8ea
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:27:36 -0300

    Update ex8.py

--- lista 1/ex8.py
+++ lista 1/ex8.py
@@ -1,3 +1,6 @@
+"8) Faça um programa que leia a largura e a altura de uma parede em metros, e calcule a sua área e a
+quantidade da tinta necessária para pinta-la, sabendo que cada litro de tinta, pinta uma área de 2m²."
+
 largura = float(input(f"Digite a largura da sua parede em metros: "))
 altura = float(input(f"Digite a altura da sua parede em metros: "))
 area = altura * altura
