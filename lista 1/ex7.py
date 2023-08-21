a03967ffc928ccb1955a44608c883314571ee70e
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 15:15:25 -0300

    ex7

---
+++ lista 1/ex7.py
@@ -0,0 +1,5 @@
+produto = float(input(f"Digite o valor do produto: "))
+desconto = produto - (produto * 0.05)
+aumento = produto + (produto * 0.15)
+print(f"O aumento de preço foi para:",aumento)
+print(f"O desconto de preço foi para:",desconto)
