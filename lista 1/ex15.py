8b0b1ac13e853a6eef839c516fecb3c9265a2eef
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 17:12:25 -0300

    ex15

---
+++ lista 1/ex15.py
@@ -0,0 +1,7 @@
+temperatura = float(input("Digite a temperatura atual:"))
+if temperatura < 20:
+    print(f"Esta uma temperatura fria")
+if temperatura > 20:
+    print(f"Esta temperatura está agradável")
+if temperatura > 30:
+    print(f"TA MUITO CALOR IRMÂO!!!!!!")
