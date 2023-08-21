677bd058b7d2aee3a75571e8486198c3d47226fb
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:29:06 -0300

    Update ex11.py

--- lista 1/ex11.py
+++ lista 1/ex11.py
@@ -1,3 +1,7 @@
+"11) Faça um programa que receba como entrada os dados de um funcionário: nome, numero de horas
+trabalhadas, valor da hora trabalhada. Após calcule seu salário bruto. Calcule um desconto de 2% de INSS. E
+ao final mostrar seu nome e salário final calculado."
+
 nome = input(f"Digite seu nome: ")
 nht = float(input(f"Digite o numero de horas trabalhadas: "))
 vlht = float(input(f"Digite o valor de horas trabalhadas: "))
