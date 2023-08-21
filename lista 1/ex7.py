ec446d28729976f5761ead1e34bd9d786903dec2
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:27:15 -0300

    Update ex7.py

--- lista 1/ex7.py
+++ lista 1/ex7.py
@@ -1,3 +1,7 @@
+"7) Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto e com 15%
+de aumento."
+
+
 produto = float(input(f"Digite o valor do produto: "))
 desconto = produto - (produto * 0.05)
 aumento = produto + (produto * 0.15)
