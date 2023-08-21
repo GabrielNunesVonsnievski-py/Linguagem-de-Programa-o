908a621f7c8a4dcc801ea0e6a449f8aecce9e7b5
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:23:58 -0300

    Update ex4.py

--- lista 1/ex4.py
+++ lista 1/ex4.py
@@ -1,3 +1,6 @@
+"4) Faça um programa que leia algo pelo teclado e mostre na tela seu tipo de dado e todas as informações sobre
+ele."
+
 dado = input("Digite algo para saber todos os seus dados:")
 print(type(dado))
 
