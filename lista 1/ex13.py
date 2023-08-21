c6f9c94c340e8448813d0a28c2fba0f238cab399
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:49:18 -0300

    ex13

---
+++ lista 1/ex13.py
@@ -0,0 +1,17 @@
+""" 13) Escreva um Programa que leia uma lista de 5 números inteiros, mostre a soma deles. """
+
+
+lista = []
+a = int(input("Digite um valor: "))
+lista.append(a)
+b = int(input("Digite um valor: "))
+lista.append(b)
+c = int(input("Digite um valor: "))
+lista.append(c)
+d = int(input("Digite um valor: "))
+lista.append(d)
+e = int(input("Digite um valor: "))
+lista.append(e)
+print(lista)
+soma = a + b + c + d + e
+print(soma)
