558b65eee5a2a02d6873ffc53b61cb9ebaf7166a
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 16:25:42 -0300

    Update ex5.py

--- lista 1/ex5.py
+++ lista 1/ex5.py
@@ -1,3 +1,4 @@
+"5) Faça um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros"
 metros = int(input("Digite um valor em Metros: "))
 centimetros = metros /100
 milimetros = metros /1000
