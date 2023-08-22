82a7b812a1b572b6de6ed04b6898f0c463d459fb
Gabriel Nunes Vonsnievski <133696902+GabrielNunesVonsnievski-py@users.noreply.github.com>
2023-08-21 22:59:16 -0300

    ex34

---
+++ lista 1/ex34.py
@@ -0,0 +1,20 @@
+""" Faça 4 listas:
+A. Filmes
+B. Jogos
+C. Livros
+D. Esporte
+a. Adicione no mínimo 2 itens em cada lista.
+b. Crie uma lista das 4 listas criadas acima.
+c. Acesse (mostrar) algum item da lista livros.
+d. Remova um item da lista esporte. """
+a = [ 'interestelar', 'tartarugas ninja' ]
+b = [ 'Valorat', 'Minecraft']
+c = [ 'Harry Potter', 'Senhor dos Anéis']
+d = [ 'Basquete' , 'Tênis']
+e = a + b + c + d
+print(e)
+print(c[0])
+print(c)
+print(d)
+del d[1]
+print(d)
